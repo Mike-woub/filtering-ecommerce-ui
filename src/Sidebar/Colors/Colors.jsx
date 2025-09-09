2a8bd69279528d373b262eb545b5f4dcf4f6f0df
Mikiyas Woubshet <mikiyaswoubshet@gmail.com>
2025-09-09 01:30:51 -0700

    Colors design

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 import './colors.css'
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+            <label className="sidebar-label-container color-title">
+                <input type="radio" name="test3" />
+                <span className='checkmark'></span>All
+            </label> 
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className='checkmark'></span>Black
+            </label> 
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className='checkmark'></span>Blue
+            </label> 
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className='checkmark'></span>Red
+            </label> 
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className='checkmark'></span>Green 
+            </label> 
+            <label className="sidebar-label-container">
+                <input type="radio" name="test2" />
+                <span className='checkmark'></span>white 
+            </label> 
+        
+    </div>
   )
 }
 
