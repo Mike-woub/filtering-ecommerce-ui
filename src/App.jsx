62a444a7f31e218927aeb596b73e098c1fb90816
Mikiyas Woubshet <mikiyaswoubshet@gmail.com>
2025-09-10 07:55:46 -0700

    Search filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const filteredItems = products.filter((product) =>
 
       //filtering Input Items
       if (query){
-        filteredProducts=filterdItems
+        filteredProducts=filteredItems
       }
 
       //Selected Filter
--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -3,10 +3,10 @@ import {FiHeart} from 'react-icons/fi'
 import {AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
 import "./nav.css";
 
-const Nav = () => {
+const Nav = ({query, handleInputChange}) => {
   return <nav>
     <div className="nav-container">
-      <input type="text" className='search-input' placeholder='Enter your search shoes'/>
+      <input type="text" className='search-input' placeholder='Enter your search shoes' onChange={handleInputChange} value={query}/>
     </div>
     <div className="profile-container">
       <a href="#">
